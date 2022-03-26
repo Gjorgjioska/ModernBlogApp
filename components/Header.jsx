a541e191004ba2ded538337b51e4e05201e28856
Gjorgjioska <marina.gorgioska@gmail.com>
2022-03-26 20:57:57 +0100

    working on the categories

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,14 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import Link from 'next/link'
+import { getCategories } from '../services'
 
-const categories = [
-  { name: 'React', slug: 'react' },
-  { name: 'Web Development', slug: 'web-dev' },
-]
 const Header = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories)
+    })
+  }, [])
   return (
     <div className="container mx-auto mb-8 px-10">
       <div className="inline-block w-full border-b border-blue-400 py-8">
